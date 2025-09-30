9191f0402c597511d5be16d609eb7896ffac7319
cocoz93 <wndnwls7@gmail.com>
2025-09-30 15:41:46 +0900

    main함수 수정

--- main.cpp
+++ main.cpp
@@ -1,27 +1,42 @@
 ﻿#pragma once
-#include "MyLanServer.h"
+#include "ChattingServer.h"
 
-#define SERVER_PORT				6000
-#define SERVER_IP				L"0.0.0.0"
-#define MAX_CLIENT				5000
-#define WORKER_TRHEAD_COUNT		10
+#define SERVER_PORT					6000
+#define SERVER_IP					L"0.0.0.0"
+#define MAX_CLIENT					15000
+#define RUN_WORKER_TRHEAD_COUNT		10
+
 
 /*
 LAN_SERVER <-> NET_SERVER
 
-1. Msg.h의 HEADER_SIZE사이즈 조절
-2. Encoding, Decoding 
+1. Msg.h의 HEADER_SIZE사이즈 조절s
+2. Encoding, Decoding
 3. Setheader
 
-TLS성능측정 CMSG말고 그냥 일반 new/delete랑 비교해보기
+파서로 설정빼기.
 */
 
-int main()
+/*
+22.5.2
+voliatile변수 추가, Release모드로 변경
+문제발견 : CMsg계속 우상향 그래프
+*/
+
+
+int main(void)
 {
-	CMyLanServer* MyLanServer = new CMyLanServer;
-	if (FALSE == MyLanServer->Start(SERVER_TYPE::LAN_SERVER, SERVER_IP, SERVER_PORT, WORKER_TRHEAD_COUNT, TRUE, MAX_CLIENT))
+	int WorkerThreadCount = 0;
+	printf("RunWorkerThread : ");
+	scanf_s("%d", &WorkerThreadCount);
+
+	// CMsg Count방식으로 증가/차감하는것
+	// Json으로 설정파일 따로 빼기
+	CChattingServer* ChattingServer = new CChattingServer;
+	if (FALSE == ChattingServer->Start(CNetServer::SERVER_TYPE::NET_SERVER, SERVER_IP, SERVER_PORT, WorkerThreadCount, TRUE, MAX_CLIENT))
 		return false;
-	
+
+	//컨텐츠
 	Sleep(INFINITE);
 
 	return 0;
